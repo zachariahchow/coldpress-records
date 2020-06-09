63dbddcfbc41fc8228a67a3bd800f8e52996f844
Zachariah Chow <zachariahchow@gmail.com>
2020-06-09 14:51:11 +0800

    feat: Add genre column to Artists table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_064952) do
+ActiveRecord::Schema.define(version: 2020_06_09_065055) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,6 +43,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_064952) do
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
     t.string "origin"
+    t.string "genre"
   end
 
   create_table "songs", force: :cascade do |t|
