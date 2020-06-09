d0c51ba07942124185401ab7e1d835d51e1a46a4
Zachariah Chow <zachariahchow@gmail.com>
2020-06-09 18:22:57 +0800

    feat: Add songs index / show endpoints

--- app/controllers/songs_controller.rb
+++ app/controllers/songs_controller.rb
@@ -1,7 +1,25 @@
 class SongsController < ApplicationController
   def index
+    @songs = Song.all
+
+    respond_to do |format|
+      format.json {
+          render :json => @songs
+      }
+    end
   end
 
   def show
+    @song = Song.find(params[:id])
+    @album = @song.album
+    @artist = @album.artist
+
+    songInfo = {song: @song, album: @album, artist: @artist}
+
+    respond_to do |format|
+      format.json {
+          render :json => songInfo
+      }
+    end
   end
 end
