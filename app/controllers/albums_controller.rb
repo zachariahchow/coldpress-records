a95bd8579531b5d9e3d2179a03a593a52b152167
Zachariah Chow <zachariahchow@gmail.com>
2020-06-09 18:20:02 +0800

    feat: Add index / show endpoints

--- app/controllers/albums_controller.rb
+++ app/controllers/albums_controller.rb
@@ -1,7 +1,24 @@
 class AlbumsController < ApplicationController
   def index
+    @albums = Album.all
+
+    respond_to do |format|
+      format.json {
+          render :json => @albums
+      }
+    end
   end
 
   def show
+    @album = Album.find(params[:id]);
+    @songs = @album.songs
+
+    albumInfo = {album: @album, songs: @songs}
+
+    respond_to do |format|
+      format.json {
+          render :json => albumInfo
+      }
+    end
   end
 end
