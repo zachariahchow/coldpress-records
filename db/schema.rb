9ed6552e1af12f08ff379db8ebd40e88b61a545d
Zachariah Chow <zachariahchow@gmail.com>
2020-06-09 14:05:24 +0800

    feat: Add title column to Songs table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_055223) do
+ActiveRecord::Schema.define(version: 2020_06_09_060447) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -54,6 +54,7 @@ ActiveRecord::Schema.define(version: 2020_06_09_055223) do
     t.text "thumbnail_img"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "title"
     t.index ["album_id"], name: "index_songs_on_album_id"
   end
 
