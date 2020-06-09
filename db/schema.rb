0f0dffec37b4ed685358cb2287642e49d762039a
Zachariah Chow <zachariahchow@gmail.com>
2020-06-09 13:24:02 +0800

    feat: Run CreateSongs migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_09_045617) do
+ActiveRecord::Schema.define(version: 2020_06_09_052328) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -43,5 +43,19 @@ ActiveRecord::Schema.define(version: 2020_06_09_045617) do
     t.datetime "updated_at", precision: 6, null: false
   end
 
+  create_table "songs", force: :cascade do |t|
+    t.bigint "album_id", null: false
+    t.integer "album_position"
+    t.text "bandcamp_url"
+    t.text "soundcloud_url"
+    t.text "youtube_url"
+    t.text "spotify_url"
+    t.text "thumbnail_img"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["album_id"], name: "index_songs_on_album_id"
+  end
+
   add_foreign_key "albums", "artists"
+  add_foreign_key "songs", "albums"
 end
