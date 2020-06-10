095eaec171487b7035e355837495a198b9cb988b
Zachariah Chow <zachariahchow@gmail.com>
2020-06-10 22:57:06 +0800

    feat: Add CartDetails X Cart / ProductOption relationships

--- app/models/cart.rb
+++ app/models/cart.rb
@@ -1,3 +1,5 @@
 class Cart < ApplicationRecord
-  belongs_to :customer
+  belongs_to :customer, optional: true
+  has_many :cart_details
+  has_many :product_options, through: :cart_details
 end
--- app/models/product_option.rb
+++ app/models/product_option.rb
@@ -1,4 +1,6 @@
 class ProductOption < ApplicationRecord
   belongs_to :product
   belongs_to :option
+  has_many :cart_details
+  has_many :carts, through: :cart_details
 end
