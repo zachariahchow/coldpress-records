7acbaf668bf22a87bf4ebc806730de544feb8c89
Zachariah Chow <zachariahchow@gmail.com>
2020-06-10 16:28:09 +0800

    feat: Run CreateOptions migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_061454) do
+ActiveRecord::Schema.define(version: 2020_06_10_082717) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,6 +50,13 @@ ActiveRecord::Schema.define(version: 2020_06_10_061454) do
   create_table "categories", force: :cascade do |t|
     t.string "category_type"
     t.text "thumbnail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "options", force: :cascade do |t|
+    t.string "name"
+    t.string "value"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
