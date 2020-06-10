acf4c3c2db905a3f08faa1b6ec81df37efae7029
Zachariah Chow <zachariahchow@gmail.com>
2020-06-10 14:15:33 +0800

    feat: Run CreateCategories migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_10_061054) do
+ActiveRecord::Schema.define(version: 2020_06_10_061454) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,13 @@ ActiveRecord::Schema.define(version: 2020_06_10_061054) do
     t.datetime "updated_at", precision: 6, null: false
     t.string "origin"
     t.string "genre"
+  end
+
+  create_table "categories", force: :cascade do |t|
+    t.string "category_type"
+    t.text "thumbnail"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
   end
 
   create_table "products", force: :cascade do |t|
