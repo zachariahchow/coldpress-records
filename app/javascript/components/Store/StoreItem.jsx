63f21c8a883736786645324774f2fa5ac8f67192
Zachariah Chow <zachariahchow@gmail.com>
2020-06-10 21:36:56 +0800

    feat: Add product option select render

--- app/javascript/components/Store/StoreItem.jsx
+++ app/javascript/components/Store/StoreItem.jsx
@@ -6,6 +6,30 @@ const StoreItem = ({ productData, productOptions, artist }) => {
 
     //
 
+    const productOptionsArr = productOptions
+        .reduce((acc, curOpt) => {
+            if (!Object.keys(acc).includes(curOpt.option_name)) {
+                acc.push({ optionName: curOpt.option_name, values: [] });
+            }
+
+            acc.find(opt => opt.optionName == curOpt.option_name).values.push(curOpt.value);
+
+            return acc;
+        }, [])
+
+    const productOptionEls = productOptionsArr.map(opt => {
+        const options = opt.values.map(value =>
+            <option value={value}>{value}</option>
+        )
+
+        return (
+            <select name={opt.optionName} id={opt.optionName}>
+                {options}
+            </select>
+        )
+    })
+
+
     return (
         <div className="store-item__container">
             <div className="store-item__header flex flex-col justify-center items-center w-full">
@@ -15,9 +39,7 @@ const StoreItem = ({ productData, productOptions, artist }) => {
                 <h2 className="store-item-artist__header-text uppercase tracking-widest text-2xl py-2">{artist.name}</h2>
             </div>
             <div className="store-item-options__container">
-                <label htmlFor="item-option">Options</label>
-                <select name="item-option" id="item-option">
-                </select>
+                {productOptionEls}
             </div>
         </div>
     );
