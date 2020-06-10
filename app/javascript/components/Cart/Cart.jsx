27a7ac5b3766b3476240e5e0c6babd185490cae7
Zachariah Chow <zachariahchow@gmail.com>
2020-06-10 23:54:52 +0800

    feat: Create Cart / CartPage component files

---
+++ app/javascript/components/Cart/Cart.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const Cart = () => {
+
+    //CSS Classes
+
+    //
+
+    return (
+        <div className="cart__container">
+        </div>
+    );
+}
+
+export default Cart;
---
+++ app/javascript/components/Cart/CartPage.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Cart from './Cart';
+
+const CartPage = () => {
+
+    //CSS Classes
+
+    //
+
+    return (
+        <div className="cart__container">
+            <Cart />
+        </div>
+    );
+}
+
+export default CartPage;
