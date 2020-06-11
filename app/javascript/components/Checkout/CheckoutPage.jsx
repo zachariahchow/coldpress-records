671d7ec159f447d19888534f1ec0477b20c46f0b
Zachariah Chow <zachariahchow@gmail.com>
2020-06-12 03:51:46 +0800

    feat: Add CheckoutPage component file

---
+++ app/javascript/components/Checkout/CheckoutPage.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+const CheckoutPage = () => {
+
+    //CSS Classes
+
+    //
+
+    return (
+        <div className="checkout-page__container w-full flex flex-col justify-center items-center">
+            Checkout Page
+        </div>
+    );
+}
+
+export default CheckoutPage;
