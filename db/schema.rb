b0a5a0fc1be9af59f5aece1dde432cd9780cfcf2
Zachariah Chow <zachariahchow@gmail.com>
2020-06-12 00:38:05 +0800

    feat: Run AddTotalPriceToOrders migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_06_11_163556) do
+ActiveRecord::Schema.define(version: 2020_06_11_163726) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -121,6 +121,7 @@ ActiveRecord::Schema.define(version: 2020_06_11_163556) do
     t.string "delivery_method"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.decimal "total_price"
   end
 
   create_table "product_options", force: :cascade do |t|
