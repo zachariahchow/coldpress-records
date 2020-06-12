e4a860ec962e284c4c77f2555a04d5d8e4694c58
Zachariah Chow <zachariahchow@gmail.com>
2020-06-12 23:21:55 +0800

    fix: Fix erroneous hash

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -3,7 +3,7 @@ class OrdersController < ApplicationController
     Order.create(customer_email: params[:customer][:email], customer_billing_address: params[:customer][:billingAddress], customer_shipping_address: params[:customer][:shippingAddress], customer_country: params[:customer][:country], customer_phone: params[:customer][:phone], status: 0, delivery_method: params[:customer][:deliveryMethod], total_price: params[:total_price])
 
     params[:order].each do |order|
-      OrderDetail.create(order_id: Order.last.id, product_option_id: order[:product_option_id], quantity: order[:quantiy], unit_price: order[:price])
+      OrderDetail.create(order_id: Order.last.id, product_option_id: order[:product_option_id], quantity: order[:quantity], unit_price: order[:price])
     end
 
     @cart.cart_details.destroy_all
