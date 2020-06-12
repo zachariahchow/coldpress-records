bdc3793ec33ff2dc86ed8db7f799be052793c05f
Zachariah Chow <zachariahchow@gmail.com>
2020-06-12 21:25:04 +0800

    fix: Fix conditional render of checkout items

--- app/javascript/components/Checkout/CheckoutPage.jsx
+++ app/javascript/components/Checkout/CheckoutPage.jsx
@@ -33,6 +33,8 @@ const CheckoutPage = ({ customerFieldChangeHandler, customerFields, cartData, co
                 return curOpt.productSubtotal + acc
             }, null)
 
+            productOptionsTallyInfo["total_amount"] = totalAmount;
+
         }
 
         //CSS Classes
@@ -47,7 +49,7 @@ const CheckoutPage = ({ customerFieldChangeHandler, customerFields, cartData, co
                         Checkout
                     </h2>
                 </div>
-                {cartData.length ? <div className="checkout-form__container w-full flex flex-col justify-center items-center">
+                {cartData.cartProducts.length ? <div className="checkout-form__container w-full flex flex-col justify-center items-center">
                     <input type="text" name="email" id="email" placeholder="Email" defaultValue={customerFields.email} onChange={(e) => customerFieldChangeHandler(e, "email")}/>
                     <input type="text" name="fullName" id="fullName" placeholder="Full Name" defaultValue={customerFields.fullName} onChange={(e) => customerFieldChangeHandler(e, "fullName")}/>
                     <input type="text" name="billingAddress" id="billingAddress" placeholder="Billing Address" defaultValue={customerFields.billingAddress} onChange={(e) => customerFieldChangeHandler(e, "billingAddress")}/>
@@ -55,12 +57,12 @@ const CheckoutPage = ({ customerFieldChangeHandler, customerFields, cartData, co
                     <input type="text" name="country" id="country" placeholder="Country" defaultValue={customerFields.country} onChange={(e) => customerFieldChangeHandler(e, "country")}/>
                     <input type="text" name="phone" id="phone" placeholder="Phone Number" defaultValue={customerFields.phone} onChange={(e) => customerFieldChangeHandler(e, "phone")}/>
                 </div> : null}
-                {cartData.length ? <div className="checkout-form-confirm__btn-container">
+                {cartData.cartProducts.length ? <div className="checkout-form-confirm__btn-container">
                     <button onClick={(e) => confirmOrderHandler(e, productOptionsTallyInfo)}className="checkout-form-confirm__btn btn-primary btn-primary bg-transparent hover:bg-gray-500 text-gray-500 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">
                         Confirm Order
                     </button>
                 </div> : null}
-                {cartData.length ? <CheckoutTally key={Math.random * 99} cartData={cartData}/> : <div className="no-items__header w-full flex flex-col justify-center items-center">
+                {cartData.cartProducts.length ? <CheckoutTally key={Math.random * 99} cartData={cartData}/> : <div className="no-items__header w-full flex flex-col justify-center items-center">
                     <p className="no-items__text text-center text-xl">
                         There are no items in your Cart.
                     </p>
