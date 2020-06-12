096f3ef273154e936850ef72f4c395fcb042abe9
Zachariah Chow <zachariahchow@gmail.com>
2020-06-12 11:00:08 +0800

    feat: Add Order create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,9 +1,7 @@
 class OrdersController < ApplicationController
   def create
-  end
-
-  def destroy
-  end
+    Order.create(customer_email: params[:customer_email], customer_billing_address: params[:customer_billing_address], customer_shipping_address: params[:customer_shipping_address], customer_country: params[:customer_country], customer_phone: params[:customer_phone], status: 0, delivery_method: params[:delivery_method], total_price: params[:total_price])
+    end
 
   def show
   end
