ef2e5e407618b2fdfc4555ea442669d072017905
Zachariah Chow <zachariahchow@gmail.com>
2020-06-13 00:57:47 +0800

    fix: Add customer full name field to create action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,6 +1,6 @@
 class OrdersController < ApplicationController
   def create
-    Order.create(customer_email: params[:customer][:email], customer_billing_address: params[:customer][:billingAddress], customer_shipping_address: params[:customer][:shippingAddress], customer_country: params[:customer][:country], customer_phone: params[:customer][:phone], status: 0, delivery_method: params[:customer][:deliveryMethod], total_price: params[:total_price])
+    Order.create(customer_email: params[:customer][:email], customer_full_name: params[:customer][:fullName], customer_billing_address: params[:customer][:billingAddress], customer_shipping_address: params[:customer][:shippingAddress], customer_country: params[:customer][:country], customer_phone: params[:customer][:phone], status: 0, delivery_method: params[:customer][:deliveryMethod], total_price: params[:total_price])
 
     params[:order].each do |order|
       OrderDetail.create(order_id: Order.last.id, product_option_id: order[:product_option_id], quantity: order[:quantity], unit_price: order[:price])
