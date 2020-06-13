558062f21713ae85fce09fd723fbc08bc536643f
Zachariah Chow <zachariahchow@gmail.com>
2020-06-13 21:16:34 +0800

    styles: Add carousel image files, adjust styles

--- app/javascript/components/LandingPage/Carousel.jsx
+++ app/javascript/components/LandingPage/Carousel.jsx
@@ -3,15 +3,27 @@ import ReactDOM from 'react-dom';
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { Carousel } from 'react-responsive-carousel';
 
-import bannerOne from '../../images/sphaeras-banner-2.jpg';
-import bannerTwo from '../../images/suneater-banner.jpg';
+import bannerOne from '../../images/atc-banner-1.jpg';
+import bannerTwo from '../../images/kc-banner-1.jpg';
+import bannerThree from '../../images/subsonic-eye-banner-1.jpg';
+import bannerFour from '../../images/hauste-banner-1.jpg';
+
 
 class LandingCarousel extends Component {
     render() {
         return (
-            <Carousel showThumbs={false} interval={5000} autoPlay={true} swipeable={true} stopOnHover={true} emulateTouch={true} showStatus={false} useKeyboardArrows={true} infiniteLoop={true} showArrows={false} transitionTime={300}>
+            <Carousel showThumbs={false} interval={6000} autoPlay={true} swipeable={true} stopOnHover={true} emulateTouch={true} showStatus={false} useKeyboardArrows={true} infiniteLoop={true} showArrows={false} transitionTime={300} dynamicHeight={true}>
                 <div>
                     <img className="object-cover" src={bannerOne} />
+                </div>
+                <div>
+                    <img className="object-cover" src={bannerTwo} />
+                </div>
+                <div>
+                    <img className="object-cover" src={bannerThree} />
+                </div>
+                <div>
+                    <img className="object-cover" src={bannerFour} />
                 </div>
             </Carousel>
         );
