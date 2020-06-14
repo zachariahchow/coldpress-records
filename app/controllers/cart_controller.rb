29c8a1f1f306e053fd28441a6eed513c7b7c5981
Zachariah Chow <zachariahchow@gmail.com>
2020-06-14 16:16:28 +0800

    fix: Fix bug where cart renders different product order on increment/decrement qty

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -1,6 +1,6 @@
 class CartController < ApplicationController
   def show
-    @cart_products = @cart.cart_details.map{ |detail|
+    @cart_products = @cart.cart_details.order(:product_option_id).map{ |detail|
       {product: ProductOption.find(detail.product_option.id).product, option: ProductOption.find(detail.product_option.id).option, product_option: ProductOption.find(detail.product_option.id)}}
 
     @customer_cart_info = {customer: @customer, cart: @cart, cartDetails: @cart.cart_details, cartProducts: @cart_products }
