2fe922c7506077cebb9b8a8dafb39be3e8d70c25
Zachariah Chow <zachariahchow@gmail.com>
2020-06-14 14:05:54 +0800

    feat: Add onClick handler to close side drawer on links

--- app/javascript/components/Layout/Nav.jsx
+++ app/javascript/components/Layout/Nav.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 
-const Nav = ({ isMenuOpen }) => {
+const Nav = ({ isMenuOpen, setIsMenuOpen }) => {
 
     //CSS classes
     const linkClasses = ['h-4 w-3/4 py-4 px-2 my-6 text-center'];
 
     return (
-        <nav className={`h-screen flex flex-col justify-center items-center fixed z-10 top-0 left-0 w-full md:w-1/2 bg-gray-200 opacity-75 ${isMenuOpen ? "transform -translate-x-0 ease-in-out duration-500" : "transform -translate-x-full ease-in-out duration-500"}`}>
-            <li className={`navlink about__link ${linkClasses.join(' ')}`}><Link to="/about">About</Link></li>
-            <li className={`navlink store__link ${linkClasses.join(' ')}`}><Link to="/store">Store</Link></li>
-            <li className={`navlink artists__link ${linkClasses.join(' ')}`}><Link to="/artists">Artists</Link></li>
-            <li className={`navlink news__link ${linkClasses.join(' ')}`}><Link to="/news">News</Link></li>
+        <nav className={`h-screen flex flex-col justify-center items-center fixed z-10 top-0 left-0 w-full md:w-1/2 bg-gray-200 opacity-75 ${isMenuOpen ? "transform -translate-x-0 ease-in-out duration-700" : "transform -translate-x-full ease-in-out duration-700"}`}>
+            <li className={`navlink about__link ${linkClasses.join(' ')}`} onClick={()=>setIsMenuOpen()}><Link to="/about">About</Link></li>
+            <li className={`navlink store__link ${linkClasses.join(' ')}`} onClick={()=>setIsMenuOpen()}><Link to="/store">Store</Link></li>
+            <li className={`navlink artists__link ${linkClasses.join(' ')}`} onClick={()=>setIsMenuOpen()}><Link to="/artists">Artists</Link></li>
+            <li className={`navlink news__link ${linkClasses.join(' ')}`} onClick={()=>setIsMenuOpen()}><Link to="/news">News</Link></li>
         </nav>
     );
 }
