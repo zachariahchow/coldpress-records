3c3af5aa71f0375101fc9347374f72a359e0d2c9
Zachariah Chow <zachariahchow@gmail.com>
2020-06-15 04:11:01 +0800

    fix: Fix bug where cart tally changes order of products on increment/decrement

--- app/controllers/cart_controller.rb
+++ app/controllers/cart_controller.rb
@@ -3,7 +3,7 @@ class CartController < ApplicationController
     @cart_products = @cart.cart_details.order(:product_option_id).map{ |detail|
       {product: ProductOption.find(detail.product_option.id).product, option: ProductOption.find(detail.product_option.id).option, product_option: ProductOption.find(detail.product_option.id)}}
 
-    @customer_cart_info = {customer: @customer, cart: @cart, cartDetails: @cart.cart_details, cartProducts: @cart_products }
+    @customer_cart_info = {customer: @customer, cart: @cart, cartDetails: @cart.cart_details.order(:product_option_id), cartProducts: @cart_products }
 
     respond_to do |format|
       format.json {
