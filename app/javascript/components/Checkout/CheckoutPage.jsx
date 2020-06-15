385abaa4b589bdbbfc11b8129e70a4bbec46855d
Zachariah Chow <zachariahchow@gmail.com>
2020-06-15 16:11:45 +0800

    styles: Update btn-primary styles, Checkout styles

--- app/javascript/components/Checkout/CheckoutPage.jsx
+++ app/javascript/components/Checkout/CheckoutPage.jsx
@@ -40,13 +40,13 @@ const CheckoutPage = ({ customerFieldChangeHandler, customerFields, cartData, co
     }
 
     //CSS Classes
-
+    const inputClasses = ['w-11/12', 'mb-4', 'px-2', 'py-2', 'border-b-2'];
     //
 
 
     return (
         <motion.div
-                    className="checkout-page__container w-full flex flex-col justify-center items-center"
+                    className="checkout-page__container w-full flex flex-col justify-center items-center mt-6"
                     initial={{opacity: 0}}
                     animate ={{opacity: 1}}
                     exit={{ opacity: 0}}
@@ -58,24 +58,30 @@ const CheckoutPage = ({ customerFieldChangeHandler, customerFields, cartData, co
                         Checkout
                     </h2>
                 </div>
-                {cartData.cartProducts.length ? <div className="checkout-form__container w-full flex flex-col justify-center items-center">
-                    <input type="text" name="email" id="email" placeholder="Email" defaultValue={customerFields.email} onChange={(e) => customerFieldChangeHandler(e, "email")}/>
-                    <input type="text" name="fullName" id="fullName" placeholder="Full Name" defaultValue={customerFields.fullName} onChange={(e) => customerFieldChangeHandler(e, "fullName")}/>
-                    <input type="text" name="billingAddress" id="billingAddress" placeholder="Billing Address" defaultValue={customerFields.billingAddress} onChange={(e) => customerFieldChangeHandler(e, "billingAddress")}/>
-                    <input type="text" name="shippingAddress" id="shippingAddress" placeholder="Shipping Address" defaultValue={customerFields.shippingAddress} onChange={(e) => customerFieldChangeHandler(e, "shippingAddress")}/>
-                    <input type="text" name="country" id="country" placeholder="Country" defaultValue={customerFields.country} onChange={(e) => customerFieldChangeHandler(e, "country")}/>
-                    <input type="text" name="phone" id="phone" placeholder="Phone Number" defaultValue={customerFields.phone} onChange={(e) => customerFieldChangeHandler(e, "phone")}/>
-                </div> : null}
-                {cartData.cartProducts.length ? <div className="checkout-form-confirm__btn-container">
-                    <button onClick={(e) => confirmOrderHandler(e, productOptionsTallyInfo, totalAmount)}className="checkout-form-confirm__btn btn-primary btn-primary bg-transparent hover:bg-gray-500 text-gray-500 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">
-                        Confirm Order
-                    </button>
-                </div> : null}
-                {cartData.cartProducts.length ? <CheckoutTally key={Math.random * 99} cartData={cartData}/> : <div className="no-items__header w-full flex flex-col justify-center items-center">
-                    <p className="no-items__text text-center text-xl">
-                        There are no items in your Cart.
-                    </p>
-                </div>}
+                <div className="checkout-form-tally__wrapper w-full flex flex-col justify-center items-center sm:flex-row sm:justify-around sm:items-start sm:mt-8 sm:px-6">
+                    {cartData.cartProducts.length ? <div className="checkout-form__container w-full flex flex-col justify-center items-center sm:w-1/2">
+                        <input className={`checkout__input ${inputClasses.join(' ')}`} type="text" name="email" id="email" placeholder="Email" defaultValue={customerFields.email} onChange={(e) => customerFieldChangeHandler(e, "email")}/>
+                        <input className={`checkout__input ${inputClasses.join(' ')}`} type="text" name="fullName" id="fullName" placeholder="Full Name" defaultValue={customerFields.fullName} onChange={(e) => customerFieldChangeHandler(e, "fullName")}/>
+                        <input className={`checkout__input ${inputClasses.join(' ')}`} type="text" name="billingAddress" id="billingAddress" placeholder="Billing Address" defaultValue={customerFields.billingAddress} onChange={(e) => customerFieldChangeHandler(e, "billingAddress")}/>
+                        <input className={`checkout__input ${inputClasses.join(' ')}`} type="text" name="shippingAddress" id="shippingAddress" placeholder="Shipping Address" defaultValue={customerFields.shippingAddress} onChange={(e) => customerFieldChangeHandler(e, "shippingAddress")}/>
+                        <input className={`checkout__input ${inputClasses.join(' ')}`} type="text" name="country" id="country" placeholder="Country" defaultValue={customerFields.country} onChange={(e) => customerFieldChangeHandler(e, "country")}/>
+                        <input className={`checkout__input ${inputClasses.join(' ')}`} type="text" name="phone" id="phone" placeholder="Phone Number" defaultValue={customerFields.phone} onChange={(e) => customerFieldChangeHandler(e, "phone")}/>
+                        {cartData.cartProducts.length ? <div className="checkout-form-confirm__btn-container">
+                            <button onClick={(e) => confirmOrderHandler(e, productOptionsTallyInfo, totalAmount)}className="checkout-form-confirm__btn btn-primary btn-primary bg-transparent hover:bg-gray-500 text-gray-500 font-semibold hover:text-white py-2 px-4 border border-gray-500 hover:border-transparent rounded">
+                                Confirm Order
+                            </button>
+                        </div> : null}
+                    </div> : null}
+
+
+                    <div className="checkout-tally__wrapper w-full sm:w-1/2 flex flex-col justify-around items-center">
+                        { cartData.cartProducts.length ? <CheckoutTally key={Math.random * 99} cartData={cartData}/> : <div className="no-items__header w-full flex flex-col justify-center items-center">
+                            <p className="no-items__text text-center text-xl">
+                                There are no items in your Cart.
+                            </p>
+                        </div> }
+                    </div>
+                 </div>
         </motion.div>
     );
 }
--- app/javascript/components/Checkout/CheckoutTally.jsx
+++ app/javascript/components/Checkout/CheckoutTally.jsx
@@ -34,21 +34,23 @@ const CheckoutTally = ({ cartData }) => {
     }, null)
 
     const tallyInfoEls = productOptionsTallyInfo.map(prod =>
-        <div className="product-tally-info__container">
-            <div className="product-tally-info-name">
-                {prod.productName}
-            </div>
-            <div className="product-tally-info-image__container">
+        <div className="product-tally-info__container w-2/3 sm:w-11/12 flex flex-col justify-around items-center mx-2 sm:flex-row sm:flex-wrap mt-6">
+            <div className="product-tally-info-image__container w-1/4 sm:w-3/4 mr-4 flex-grow-0 mb-2">
                 <img src={prod.optionImage} alt=""/>
             </div>
-            <div className="product-tally-info-product-option">
-                {prod.productOption}
-            </div>
-            <div className="product-tally-info-quantity">
-                Quantity: {prod.quantity}
-            </div>
-            <div className="product-tally-info-subtotal">
-                Subtotal: {prod.productSubtotal}
+            <div className="product-tally-info-text__container flex flex-col justify-around items-center text-xs flex-grow-1">
+                <div className="product-tally-info-name w-full flex justify-center items-center">
+                    <p className="product-tally-info-name__text text-center">{prod.productName}</p>
+                </div>
+                <div className="product-tally-info-product-option">
+                    {prod.productOption}
+                </div>
+                <div className="product-tally-info-quantity">
+                    Quantity: {prod.quantity}
+                </div>
+                <div className="product-tally-info-subtotal">
+                    Subtotal: {prod.productSubtotal}
+                </div>
             </div>
         </div>
     )
@@ -57,7 +59,7 @@ const CheckoutTally = ({ cartData }) => {
         <div className="tally__container w-full flex flex-col justify-center items-center">
             {tallyInfoEls}
             <div className="tally-total__container">
-                <h2 className="tally-total__text">
+                <h2 className="tally-total__text mt-8 mb-4">
                     Total: {totalAmount}
                 </h2>
             </div>
