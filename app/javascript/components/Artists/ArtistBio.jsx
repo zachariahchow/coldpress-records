6f52df65351b5287068565627ca683b8cdce1b40
Zachariah Chow <zachariahchow@gmail.com>
2020-06-16 02:34:58 +0800

    styles: Update ArtistBio styles

--- app/javascript/components/Artists/ArtistBio.jsx
+++ app/javascript/components/Artists/ArtistBio.jsx
@@ -7,9 +7,9 @@ const ArtistBio = ({ artistData, match }) => {
 
     const [show, set] = useState(false)
     const transitions = useTransition(show, null, {
-        from: { transform: 'translateY(200%)' },
+        from: { transform: 'translateY(100%)' },
         enter: { transform: 'translateY(0)' },
-        leave: { transform: 'translateY(200%)' },
+        leave: { transform: 'translateY(100%)' },
     })
 
     const [artistAlbums, setArtistAlbums] = useState([]);
@@ -63,16 +63,21 @@ const ArtistBio = ({ artistData, match }) => {
             <p className="artist-bio__text font-serif text-center w-11/12 my-2">{artistData.bio}</p>
             </div>
             <div className="artist-bio-discography__container flex flex-col justify-around items-center p-2 w-full">
-                <h2 className="artist-bio-discography__header">Releases</h2>
-
-            {artistAlbums.map(album =>
-                <div className="artist-bio-album__container">
-                    <div className="album-container flex flex-col justify-center items-center w-auto mx-4">
-                        <p className="album-name__text text-center my-2 flex flex-col justify-center items-center"><span className="italic">{album.name}</span> <span className="font-bold">{album.artist_name}</span></p>
-                        <iframe className="latest-albums__iframe" src={`https://bandcamp.com/EmbeddedPlayer/${album.bandcamp_album_code}/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/`} seamless><a href={album.bandcamp_url}>{album.name}</a></iframe>
+                <h2 className="artist-bio-discography__header my-8 text-xl tracking-widest">Releases</h2>
+            <div className="artist-albums__container px-2 w-11/12 flex md:flex-row md:justify-center md:items-around flex-col flex-wrap justify-around items-center">
+                {artistAlbums.map(album =>
+                    <div
+                        className="album-container flex flex-col justify-center items-center w-1/2 lg:w-auto mx-4 mb-6 transform hover:scale-105 "
+                        key={artistAlbums.indexOf(album) + 999}
+                    >
+                        <p className="album-name__text text-center py-4 flex flex-col justify-center items-center"><span className="uppercase text-xl tracking-wider font-semibold">{album.name}</span> <span className="font-bold border-b-2 border-white">{album.artist_name}</span></p>
+                        <iframe className="latest-albums__iframe" src={`https://bandcamp.com/EmbeddedPlayer/${album.bandcamp_album_code}/size=large/bgcol=ffffff/linkcol=0687f5/tracklist=false/transparent=true/`}
+                            seamless>
+                            <a href={album.bandcamp_url}>{album.name}</a>
+                        </iframe>
                     </div>
-                </div>
-            )}
+                )}
+            </div>
 
             </div>
         </animated.div>
