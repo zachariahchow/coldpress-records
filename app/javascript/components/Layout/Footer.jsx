4e35fd79207614b17e6ecc7eab86e6dc407d88cf
Zachariah Chow <zachariahchow@gmail.com>
2020-06-16 00:08:00 +0800

    feat: Add Footer component

---
+++ app/javascript/components/Layout/Footer.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const Footer = () => {
+
+    //CSS classes
+    //
+
+    return (
+        <footer>
+            <ul className="footer-links__container"></ul>
+        </footer>
+    );
+}
+
+export default Footer;
